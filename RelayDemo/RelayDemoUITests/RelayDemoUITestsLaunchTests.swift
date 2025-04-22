3ed9d62a0b4b9dee06e72972f5b0fc15a1ce167d
Shaxzod <shaxzodsunnatov23@gmail.com>
2025-04-22 19:04:46 +0500

    init Demo app

---
+++ RelayDemo/RelayDemoUITests/RelayDemoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  RelayDemoUITestsLaunchTests.swift
+//  RelayDemoUITests
+//
+//  Created by Shaxzod on 22/04/25.
+//
+
+import XCTest
+
+final class RelayDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
